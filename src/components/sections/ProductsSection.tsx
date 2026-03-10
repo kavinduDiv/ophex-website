dfe64b0620415153a65c6c62a20cd0b31078d810
Prabuddhi-Rathnayake <prabuddhi.wemixt@gmail.com>
2026-03-10 22:54:12 +0530

    checkpoint 5

--- src/components/sections/ProductsSection.tsx
+++ src/components/sections/ProductsSection.tsx
@@ -78,9 +78,9 @@ const ProductsSection = () => {
   });
 
   return (
-    <section id="products" className="py-24 bg-background relative overflow-hidden">
+    <section id="products" className="py-24 bg-background relative">
       {/* Background Decor */}
-      <div className="absolute inset-0 pointer-events-none">
+      <div className="absolute inset-0 pointer-events-none overflow-hidden">
         <div className="absolute -top-20 -left-20 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
         <div className="absolute top-1/2 -right-20 w-80 h-80 bg-blue-500/5 rounded-full blur-3xl" />
       </div>
