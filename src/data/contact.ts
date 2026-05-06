70672907c99acac4b39892d3a311df39564b49ee
kavindu202 <kavindu.sasanka962@gmail.com>
2026-05-06 19:21:07 +0530

    updates

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,9 +1,9 @@
 export const contactData = {
     address: "Colombo road, Gampaha,\nSri Lanka",
-    phone: "+94 78 883 4962",
-    phoneFormatted: "+94788834962", // Ideal for tel: links
+    phone: "+94 72 468 0135",
+    phoneFormatted: "+94724680135", // Ideal for tel: links
     email: "info@ophexsoftware.com",
-    whatsapp: "94788834962", // Formatted without '+' for wa.me links
+    whatsapp: "94724680135", // Formatted without '+' for wa.me links
     businessHours: "Mon - Fri: 9:00 AM - 6:00 PM\nSat: 10:00 AM - 4:00 PM",
 
     // Social Links
